9c364133a4465013c8eb32b34469b667834deaca
JOSHUA AMADOR LARA <joshyarit07@gmail.c.om>
2023-04-19 17:49:04 -0600

    Se agrego 2 metodos

--- PrimerProyecto/Curso.cpp
+++ PrimerProyecto/Curso.cpp
@@ -1,7 +1,14 @@
 #include "Curso.h"
 
 Curso::Curso()
-	:nombre(""), descripcion(""), nivel(""), cupoMaximo(0) {}
+	:nombre(""), descripcion(""), nivel(""), cupoMaximo(0) {
+	reserva = new Deportista * [tamD];
+	horario = new Horario * [7];
+	for (int i = 0; i < tamD; i++)
+		reserva[i] = NULL;
+	for (int i = 0; i < 7; i++)
+		horario[i] = NULL;
+}
 Curso::Curso(string nom, string desc, string niv, int cupo)
 	:nombre(nom), descripcion(desc), nivel(niv), cupoMaximo(cupo), tamD(cupo), cantD(0), cantH(0){
 	reserva = new Deportista * [tamD];
