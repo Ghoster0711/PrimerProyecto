f5908b39f531ba8cb90ceafce7f17f76c30f43ae
JOSHUA AMADOR LARA <joshyarit07@gmail.c.om>
2023-04-19 20:54:49 -0600

    Creacion de la clase "Nodo"

---
+++ PrimerProyecto/Nodo.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Deportista.h"
+#include "Curso.h"
+
+template<class T>
+class Nodo {
+private:
+	T* dato;
+	Nodo<T>* siguiente;
+public:
+	Nodo(T*);
+	virtual ~Nodo();
+
+	T* getDato();
+	Nodo<T>* getSiguiente();
+
+	void setDato(T*);
+	void setSiguiente(Nodo<T>*);
+};
