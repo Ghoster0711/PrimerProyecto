ee04645d7133a49c34eb2d6547d4d051af707691
SIANNY BARRANTES BENAVIDES <sianny.barrantes.benavides@est.una.ac.cr>
2023-04-19 19:09:19 -0600

    c

--- PrimerProyecto/ServicioFecha.h
+++ PrimerProyecto/ServicioFecha.h
@@ -1,1 +1,14 @@
 #pragma once
+#include"Fecha.h"
+
+class ServicioFechas {
+private:
+	Fecha* fecha1;
+	Fecha* fecha2;
+public:
+	ServicioFechas(Fecha*, Fecha*);
+	virtual ~ServicioFechas();
+	string diferenciaFechas();
+	string calculoEdad();
+	string toString();
+};
--- PrimerProyecto/ServicioFechas.cpp
+++ PrimerProyecto/ServicioFechas.cpp
@@ -1,13 +1,13 @@
-#include"Fecha.h"
+#include"ServicioFecha.h"
 
-class ServicioFechas {
-private:
-	Fecha* fecha1;
-	Fecha* fecha2;
-public:
-	ServicioFechas(Fecha*, Fecha*);
-	virtual ~ServicioFechas();
-	string diferenciaFechas();
-	string calculoEdad();
-	string toString();
-};
+
+ServicioFechas::ServicioFechas(Fecha* f1, Fecha* f2) {
+	fecha1 = f1;
+	fecha2 = f2;
+}
+ServicioFechas:: ~ServicioFechas(){}
+string ServicioFechas::diferenciaFechas(){
+	
+}
+string ServicioFechas::calculoEdad(){}
+string ServicioFechas::toString(){}
