a72592926c24218c32710fa8cb4aa291acbb87bc
SIANNY BARRANTES BENAVIDES <sianny.barrantes.benavides@est.una.ac.cr>
2023-04-19 20:37:06 -0600

    Diferencia de fecha

--- PrimerProyecto/ServicioFechas.cpp
+++ PrimerProyecto/ServicioFechas.cpp
@@ -1,13 +1,38 @@
 #include"ServicioFecha.h"
 
 
-ServicioFechas::ServicioFechas(Fecha* f1, Fecha* f2) {
-	fecha1 = f1;
-	fecha2 = f2;
+string ServicioFechas::diferenciaFechas(Fecha* fecha1, Fecha* fecha2) {
+	int respFech, respMes;
+	int f1 = 0, m1 = 0, a1 = 0;
+	stringstream s;
+
+	if (fecha1->getDia() > fecha2->getDia()) {
+		f1 = fecha1->getDia() + 30;
+		m1 = fecha1->getMes() - 1;
+		respFech = fecha1->getDia() - fecha2->getDia();
+	}
+	else {
+		respFech = fecha1->getDia() - fecha2->getDia();
+		if (fecha1->getMes() < fecha2->getMes()) {
+			m1 = fecha1->getMes() + 12;
+			a1 = fecha1->getAnio() - 1;
+			respMes = fecha1->getMes() - fecha2->getMes();
+		}
+		else
+			respMes = fecha1->getMes() - fecha2->getMes();
+	}
+	s << "La diferencia entre " << fecha1->toString() << " y " << fecha2->toString() 
+		<< " es de: ";
+	if (fecha1->getAnio() - fecha2->getAnio() <= 0) {
+		s << respMes << " meses " << " y " << respFech << " dias" << endl;
+	}
+	else {
+		s << fecha1->getAnio() - fecha2->getAnio()
+			<< " años, con " << respMes << " meses " << " y " << respFech << " dias" << endl;
+	}
+	return s.str();
+
 }
-ServicioFechas:: ~ServicioFechas(){}
-string ServicioFechas::diferenciaFechas(){
-	
-}
-string ServicioFechas::calculoEdad(){}
-string ServicioFechas::toString(){}
+//string ServicioFechas::calculoEdad(Fecha* fecha1,Fecha* fecha2){
+//	
+//} 
