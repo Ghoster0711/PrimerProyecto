fd9d1262e0bb282c8fe86d7b7ba1fd6fa11ddf6d
JOSHUA AMADOR LARA <joshyarit07@gmail.c.om>
2023-04-23 16:30:51 -0600

    a

--- PrimerProyecto/Grupo.h
+++ PrimerProyecto/Grupo.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <iostream>
+#include <sstream>
+#include "Triatlonista.h"
+#include "Horario.h"
+#include "Fecha.h"
+#include "Instructor.h"
+
+using namespace std;
+
+class Grupo {
+private:
+	string instructorID();
+	string nombreInstructor();
+
+public:
+
+};
