d0e450d6764d9308f6803418b92903e0c547789e
JOSHUA AMADOR LARA <joshyarit07@gmail.c.om>
2023-04-23 16:53:19 -0600

    .

--- PrimerProyecto/Grupo.h
+++ PrimerProyecto/Grupo.h
@@ -4,14 +4,17 @@
 #include "Triatlonista.h"
 #include "Horario.h"
 #include "Fecha.h"
-#include "Instructor.h"
 
 using namespace std;
 
 class Grupo {
 private:
-	string instructorID();
-	string nombreInstructor();
+	string instructorID;
+	string nombreInstructor;
+	int cupoMaximo;
+	int duracion;
+	Horario* horario;
+	Fecha* fecInicio;
 
 public:
 
