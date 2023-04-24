376e0bf3185368ce02a11d599e391367e07d8fe3
JOSHUA AMADOR LARA <joshyarit07@gmail.c.om>
2023-04-23 23:25:12 -0600

    Se arreglaron los problemas

--- PrimerProyecto/Nodo.h
+++ PrimerProyecto/Nodo.h
@@ -19,3 +19,31 @@ public:
 	void setDato(T*);
 	void setSiguiente(Nodo<T>*);
 };
+
+template<class T>
+Nodo<T>::Nodo() {
+	this->dato = NULL;
+	this->siguiente = NULL;
+}
+
+template<class T>
+Nodo<T>::Nodo(T* dato) {
+	this->dato = dato;
+	this->siguiente = NULL;
+}
+
+template<class T>
+Nodo<T>::~Nodo() {
+}
+
+template<class T>
+T* Nodo<T>::getDato() { return dato; }
+
+template<class T>
+Nodo<T>* Nodo<T>::getSiguiente() { return siguiente; }
+
+template<class T>
+void Nodo<T>::setDato(T* dato) { this->dato = dato; }
+
+template<class T>
+void Nodo<T>::setSiguiente(Nodo<T>* siguiente) { this->siguiente = siguiente; }
