32c5a37c35a91ce0f2c3bde8850780c9514d2d77
SIANNY BARRANTES BENAVIDES <sianny.barrantes.benavides@est.una.ac.cr>
2023-04-26 13:25:02 -0600

    .

--- PrimerProyecto/ExcepcionGrupoLleno.cpp
+++ PrimerProyecto/ExcepcionGrupoLleno.cpp
@@ -2,6 +2,6 @@
 
 ExcepcionGrupoLleno::ExcepcionGrupoLleno(): Excepciones() {}
 ExcepcionGrupoLleno::~ExcepcionGrupoLleno(){}
-string ExcepcionGrupoLleno::toString() const{
+string ExcepcionGrupoLleno::toString() const {
 	return "ERROR: el grupo digido no posee campos disponibles.\n";
 }
